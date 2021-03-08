aef8d9d07e1c2c7122eb9e8a422830d2465adc39
Asaf Cohen <AsafCoh3n@protonmail.com>
2021-03-08 13:11:40 +0200

    Update asafos_ctrl.cs

--- asafos_ctrl.cs
+++ asafos_ctrl.cs
@@ -6,20 +6,21 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Threading;
 using System.Net;
-using System.Timers;
 class Program
 {
     static void Main(string[] args)
     {
 
-        IPAddress ip = IPAddress.Parse("10.0.0.11");
-        int port = 80;
+        IPAddress ip = IPAddress.Parse("10.0.0.64");
+        int port = 29000;
         TcpClient client = new TcpClient();
         Console.WriteLine("trying to connect to asafos");
         client.Connect(ip, port);
-        Console.WriteLine("asafos connected!!");
+        Console.WriteLine("asafos connected!!\ntype {>?} for help menu");
         NetworkStream ns = client.GetStream();
         Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+        client.ReceiveTimeout = 5000000;
+
 
         thread.Start(client);
         string s;
@@ -50,16 +51,11 @@ class Program
 
         while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
         {
-
-            if (DateTime.Now > endRunAt)
-            {
-                char[] input = Encoding.ASCII.GetChars(receivedBytes, 0, byte_count);
-                var onlyLetters = new String(input.SkipWhile(p => !Char.IsLetter(p) && !Char.IsDigit(p)).ToArray());
-                Console.WriteLine("$ "+ onlyLetters + " $");
-               // Console.WriteLine(input);
-
-                endRunAt = DateTime.Now.AddSeconds(1);
-            }
+			
+            char[] input = Encoding.ASCII.GetChars(receivedBytes, 0, byte_count);
+            var onlyLetters = new String(input.SkipWhile(p => !Char.IsLetter(p) && !Char.IsDigit(p)).ToArray());
+        
+			      Console.WriteLine("${0}",input);//raw input without filter
 
 
         }
